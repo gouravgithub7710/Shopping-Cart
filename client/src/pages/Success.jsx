0fbd8eb8f0379f242654563629c920896a1f2952
Gourav Giri Goswami <gouravgiri2004@gmail.com>
2026-01-31 21:53:05 +0530

    Update Success.jsx

--- client/src/pages/Success.jsx
+++ client/src/pages/Success.jsx
@@ -1,32 +1,35 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-
 import Loader from '../components/Loader'
 
-const Success = ({getUser}) => {
-  
-  const [loading,setLoading] = useState(true)
+const Success = ({ getUser }) => {
+  const [loading, setLoading] = useState(true)
   const navigate = useNavigate()
- 
-  const clearCart = async() =>{
-    
-    const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/cart/clear`,{
-      credentials:'include'
-    })
-    const data = await res.json()
-    if(data.success){
-      getUser()
-      navigate("/success")
+
+  const clearCart = async () => {
+    try {
+      const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/cart/clear`, {
+        credentials: 'include'
+      })
+      const data = await res.json()
+      if (data.success) {
+        getUser()
+        navigate("/")
+      }
+    } catch (error) {
+      console.log(error)
     }
     setLoading(false)
   }
-  useEffect(()=>{
+
+  useEffect(() => {
     clearCart()
-  },[])
-  if(loading){
-    return <Loader/>
+  }, [])
+
+  if (loading) {
+    return <Loader />
   }
-  
+
   return (
     <div className='h-screen w-full flex items-center justify-center'>
       <h2 className='text-2xl font-bold text-green-500'>Order Successfull.</h2>
@@ -35,3 +38,42 @@ const Success = ({getUser}) => {
 }
 
 export default Success
+
+
+// import React, { useEffect, useState } from 'react'
+// import { useNavigate } from 'react-router-dom'
+
+// import Loader from '../components/Loader'
+
+// const Success = ({getUser}) => {
+  
+//   const [loading,setLoading] = useState(true)
+//   const navigate = useNavigate()
+ 
+//   const clearCart = async() =>{
+    
+//     const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/cart/clear`,{
+//       credentials:'include'
+//     })
+//     const data = await res.json()
+//     if(data.success){
+//       getUser()
+//       navigate("/success")
+//     }
+//     setLoading(false)
+//   }
+//   useEffect(()=>{
+//     clearCart()
+//   },[])
+//   if(loading){
+//     return <Loader/>
+//   }
+  
+//   return (
+//     <div className='h-screen w-full flex items-center justify-center'>
+//       <h2 className='text-2xl font-bold text-green-500'>Order Successfull.</h2>
+//     </div>
+//   )
+// }
+
+// export default Success
