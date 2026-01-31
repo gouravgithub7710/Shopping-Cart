7eed4675d6a86aaacf97b3d38f5e0399839cc71f
Gourav Giri Goswami <gouravgiri2004@gmail.com>
2026-01-31 21:51:28 +0530

    Update getUserFromServer.js

--- client/src/helpers/getUserFromServer.js
+++ client/src/helpers/getUserFromServer.js
@@ -1,10 +1,24 @@
-const getUserFromServer = async() =>{
-    const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/auth/user`,{
-        credentials:'include'
-    })
-    const data = await res.json()
-
-    return data
+const getUserFromServer = async () => {
+    try {
+        const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/auth/user`, {
+            credentials: 'include'
+        })
+        const data = await res.json()
+        return data
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 export default getUserFromServer
+
+// const getUserFromServer = async() =>{
+//     const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/auth/user`,{
+//         credentials:'include'
+//     })
+//     const data = await res.json()
+
+//     return data
+// }
+
+// export default getUserFromServer
